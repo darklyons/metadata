60383a7451373f9714f921e24e1ec96f1d9aa4ec
Peter Lyons <darklion@peterlyons.id.au>
2015-07-01 06:28:00 +0000

    Clean up the initial code in preparation for further development.
    git-svn-id: svn+ssh://darklion.net/src/base/metadata@993 58635e53-d00f-0410-b943-d8d70788eb35

--- meta.py
+++ meta.py
@@ -10,26 +10,31 @@
 #	Peter Lyons
 # IMPLEMENTATION
 #	Use the tvdb_api module.
+# GLOBALS
+#	tvdb		database connection object
 #
 
 
 """TV metadata utility."""
+__title__ = "TVDB.com Query Utility"
 __author__ = "darklion"
-__version__ = "0.1"
-
+__version__ = "0.1.1"
+# Version 0.1	Initial development
+# Version 0.1.1	Cleanup for further development
 
 # Import modules
 import sys
 from tvdb_api import Tvdb
-
-tvdb = Tvdb(interactive=True, cache=True)
 
 # Check arguments
 if len(sys.argv) != 2:
     sys.stderr.write("%s: Must supply exactly one argument!\n" % ( __file__))
     sys.exit(1)
 
-# Process arguments
+# Connect to database
+tvdb = Tvdb(interactive=True, cache=True)
+
+# Process query
 seriesname = sys.argv[1]
 show = tvdb[seriesname]
 print show['seriesname'] + ":"
